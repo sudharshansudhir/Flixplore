dc83c5fe7bbaf8b9ad96b4d2cae49ce8e4337c32
sudharshansudhir <sudhir.ssr7@gmail.com>
2025-10-25 10:32:55 +0530

    Routing for Pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Route,Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Menu from './pages/Menu'
+import Cart from './pages/Cart'
+import Shops from './pages/Shops'
+import Profile from './pages/Profile'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Location from './pages/Location'
+import About from './pages/About'
+import Contact from './pages/Contact'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route path='/' Component={Home}></Route>
+        <Route path='/menu' Component={Menu}></Route>
+        <Route path='/shops' Component={Shops}></Route>
+        <Route path='/cart' Component={Cart}></Route>
+        <Route path='/location' Component={Location}></Route>
+        <Route path='/login' Component={Login}></Route>
+        <Route path='/register' Component={Register}></Route>
+        <Route path='/profile' Component={Profile}></Route>
+        <Route path='/about' Component={About}></Route>
+        <Route path='/contact' Component={Contact}></Route>
+      </Routes>
     </>
   )
 }
